b6b27a64992849cfb7aa3dd1fcb888c5e39fb9a0
Kadir ADIMUTLU <kadiradimutlu@gmail.com>
2026-04-28 21:28:34 +0300

    feat: added GameManager subscriber to handle core destruction

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void OnEnable()
+    {
+        EnergyCore.OnCoreDestroyed += HandleGameOver;
+    }
+
+    void OnDisable()
+    {
+        EnergyCore.OnCoreDestroyed -= HandleGameOver;
+    }
+
+    private void HandleGameOver()
+    {
+        Debug.Log("GAME OVER! The core has been breached.");
+        [cite_start]
+    }
+}
