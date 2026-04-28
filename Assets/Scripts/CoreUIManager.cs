4dcbd1ae7b0c71c8296d32540dccbb2bb6f48594
Kadir ADIMUTLU <kadiradimutlu@gmail.com>
2026-04-28 21:27:15 +0300

    feat: added CoreUIManager as a subscriber to core health events

---
+++ Assets/Scripts/CoreUIManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using TMPro;
+
+public class CoreUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI healthText;
+
+    void OnEnable()
+    {
+        EnergyCore.OnCoreHealthChanged += UpdateHealthUI;
+    }
+
+    [cite_start]
+    void OnDisable()
+    {
+        EnergyCore.OnCoreHealthChanged -= UpdateHealthUI;
+    }
+
+    private void UpdateHealthUI(int currentHealth, int maxHealth)
+    {
+        if (healthText != null)
+        {
+            healthText.text = "CORE HP: " + currentHealth + " / " + maxHealth;
+            Debug.Log("UI Updated: " + currentHealth);
+        }
+    }
+}
