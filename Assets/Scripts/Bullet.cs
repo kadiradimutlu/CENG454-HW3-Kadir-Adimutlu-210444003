bc423a2f0ac7ac34fda703670e39250bbf4d35b9
Kadir ADIMUTLU <kadiradimutlu@gmail.com>
2026-05-11 19:19:24 +0300

    fix: prevent active bullet reuse in object pool

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,37 +1,54 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public float speed = 50f;
     public int damage = 10;
-    public float lifeTime = 2f; 
+    public float lifeTime = 2f;
 
     private float timer;
+    private TrailRenderer[] trailRenderers;
 
-    void OnEnable()
+    private void Awake()
     {
-        timer = 0f; 
+        trailRenderers = GetComponentsInChildren<TrailRenderer>();
     }
 
-    void Update()
+    private void OnEnable()
+    {
+        timer = 0f;
+
+        if (trailRenderers != null)
+        {
+            foreach (TrailRenderer trail in trailRenderers)
+            {
+                if (trail != null)
+                {
+                    trail.Clear();
+                }
+            }
+        }
+    }
+
+    private void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         timer += Time.deltaTime;
         if (timer >= lifeTime)
         {
-            gameObject.SetActive(false); 
+            gameObject.SetActive(false);
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         IDamageable target = other.GetComponent<IDamageable>();
         if (target != null)
         {
             target.TakeDamage(damage);
         }
-        
-        gameObject.SetActive(false); 
+
+        gameObject.SetActive(false);
     }
 }
